847e031f352ec8d71f777bf60269768839c598a2
Daniel Enriquez <daniel.enriquezmontalvo@gmail.com>
2024-08-26 09:42:31 -0700

    base

---
+++ src/app/gastiganto/pages/plazas/plazas-list/plazas-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plazas-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './plazas-list.component.html',
+  styleUrl: './plazas-list.component.scss'
+})
+export class PlazasListComponent {
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+
+    production: true,
+
+    api :  {
+        baseUrl : '/v1/api/web',
+        authTokenName: '__gastigantotoken__',
+        storageUrl : '/storage',
+
+    },
+
+};
